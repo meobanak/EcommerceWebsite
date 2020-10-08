f333c7ed93d8980389ecce29afac4a5415b0adba
meobanak113 meobanak113 <meobanak8@gmail.com>
2020-10-08 22:20:33 +0700

    add IDataProvider

---
+++ Exam1/Service/Interface/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebsite.Service.Interface
+{
+    public interface IDataProvider
+    {
+        IList<T> QueryForList<T>();
+        T QueryForObject<T>(IDictionary<string,object> param);
+    }
+}
